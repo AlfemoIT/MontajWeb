998da0a45e1ecb3d6a2e263a328506e1709c4d7b
Siyar Ozturk <siyarozturk@alfemo.com.tr>
2025-02-21 08:05:47 +0300

    Add project files.

---
+++ MontajWeb/Controllers/PdfReadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MontajWeb.Controllers
+{
+    public class PdfReadController : Controller
+    {
+        // GET: PdfRead
+        public ActionResult Index(string productid)
+        {
+            return RedirectToAction("Index","Home",new { productid = productid });
+        }
+    }
+}
---
+++ MontajWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MontajWeb.MvcApplication" Language="C#" %>
